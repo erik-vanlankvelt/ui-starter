7ca7be3229e25a5f30c5209bf03e20d9e9cac5db
Erik Van Lankvelt <evanlankvelt@gmail.com>
2020-02-09 16:01:59 -0700

    Add: Sass integration

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # ui-starter
-UI starter project.
+
+## Overview
+Starter UI project.
+- [Babel](https://github.com/babel/babel)
+- [Webpack](https://github.com/webpack/webpack)
+- [React](https://github.com/facebook/react).
+- [Sass](https://github.com/sass/sass)
+
+## Setup
+##### Install homebrew (if not done already)
+```
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+```
+
+##### Install node (if not done already)
+```
+brew install node
+```
+
+##### Install npm modules
+```
+npm install
+```
+
+## Available Scripts
+
+In the project directory, you can run:
+
+```
+npm start
+```
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.
+
+```
+npm run build
+```
+Builds the app for production to the `dist` folder.
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  // entry: Where the application starts and where to start bundling files.
+  entry: "./src/index.js",
+  mode: "development",
+  /* module: Defines how exported javascript modules are transformed 
+  and which ones are included according to the given array of rules. */
+  module: {
+    rules: [
+      {
+        // Use babel to transform our ES6 and JSX syntax (.js and .jsx files).
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+      },
+      {
+        /* style-loader: Creates `style` nodes from JS strings.
+        css-loader: Translates CSS into CommonJS.
+        sass-loader: Compiles Sass to CSS. */
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  /* resolve: specify which extensions webpack will resolve and
+  import modules without needing to add their extensions. */
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  // output: tells webpack where to put our bundled code.
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    /* publicPath: specifies what directory the bundle should go in and
+    tells webpack-dev-server where to serve files from.  
+    It also specifies the public URL of the the directory. */
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  /* devServer: the location we’re serving static files from
+  and the port we want to run the server on. */
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    // publicPath: tells the server where the bundled code is.
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true
+  },
+  // HotModuleReplacement: Automatic refreshes after changes.
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
