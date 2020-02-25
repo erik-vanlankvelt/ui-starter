1dc4891e83bf51981fb6857dda1710f599308cf1
Erik Van Lankvelt <evanlankvelt@gmail.com>
2020-02-25 10:53:04 -0700

    Update: webpack to hash bundled files and bundle css, jest config to not check scss files

--- README.md
+++ README.md
@@ -43,7 +43,7 @@ npm start
 ```
 
 Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Open [http://localhost:8080](http://localhost:8080) to view it in the browser.
 
 The page will reload if you make edits.
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,72 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  // entry: Where the application starts and where to start bundling files.
-  entry: "./src/index.js",
-  mode: "development",
-  /* module: Defines how exported javascript modules are transformed 
+    // entry: Where the application starts and where to start bundling files.
+    entry: './src/index.js',
+    mode: 'development',
+    /* module: Defines how exported javascript modules are transformed 
   and which ones are included according to the given array of rules. */
-  module: {
-    rules: [
-      {
-        // Use babel to transform our ES6 and JSX syntax (.js and .jsx files).
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
-      },
-      {
-        /* style-loader: Creates `style` nodes from JS strings.
-        css-loader: Translates CSS into CommonJS.
-        sass-loader: Compiles Sass to CSS. */
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
-  /* resolve: specify which extensions webpack will resolve and
-  import modules without needing to add their extensions. */
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  // output: tells webpack where to put our bundled code.
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    /* publicPath: specifies what directory the bundle should go in and
-    tells webpack-dev-server where to serve files from.  
-    It also specifies the public URL of the the directory. */
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  /* devServer: the location we’re serving static files from
+    module: {
+        rules: [
+            {
+                // Use babel to transform our ES6 and JSX syntax (.js and .jsx files).
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: { presets: ['@babel/preset-env', '@babel/preset-react'] }
+            },
+            {
+                /* style-loader: Creates `style` nodes from JS strings.
+          css-loader: Translates CSS into CommonJS.
+          sass-loader: Compiles Sass to CSS. */
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    /* resolve: specify which extensions webpack will resolve and
+    import modules without needing to add their extensions. */
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+        modules: ['node_modules', 'src']
+    },
+    // output: tells webpack where to put our bundled code.
+    output: {
+        /* [name].[hash] keeps it unique to prevent browser caching,
+      hash will change each chunk across each build */
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist/')
+    },
+    /* devServer: the location we’re serving static files from
   and the port we want to run the server on. */
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    // publicPath: tells the server where the bundled code is.
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  // HotModuleReplacement: Automatic refreshes after changes.
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+    /* devServer: the location we’re serving static files from
+  and the port we want to run the server on. */
+    devServer: {
+        compress: true,
+        // contentBase is where static html files are placed
+        contentBase: path.join(__dirname, 'public/'),
+        historyApiFallback: true,
+        hotOnly: true,
+        port: 8080,
+        // publicPath: tells the server where the bundled code is.
+        publicPath: '/',
+        watchContentBase: true
+    },
+    plugins: [
+        // HotModuleReplacement: Automatic refreshes after changes.
+        new webpack.HotModuleReplacementPlugin(),
+        /* Extracts all CSS into one file, [name].[hash] keeps it unique to prevent browser caching
+    will change each chunk across each build
+    */
+        new MiniCssExtractPlugin({ filename: '[name].[hash].css' }),
+        // Creates HTML file to serve webpack bundles
+        new HtmlWebpackPlugin({
+            title: 'UI Starter Project',
+            template: './public/index.html',
+            filename: 'index.html'
+        })
+    ]
 };
